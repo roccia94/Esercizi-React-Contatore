40f3fff4a9c4693fd282ea520e307696d2ea40f6
Edoardo Roccetti <edoardo.roccetti@magespecialist.it>
2020-11-24 11:35:42 +0100

    Esercizio Contatore React JS

---
+++ componente/Contatore.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import {useState} from "react";
+import ShowCounterData from "./ShowCounterData";
+
+function Contatore (props){
+    const { valueStart } = props;
+    const [ counter, setCounter] = useState(valueStart);
+    const increment = () => {
+        setCounter(counter + 1);
+    }
+
+    const decrease = () => {
+        setCounter(counter - 1);
+    }
+
+    return (
+        <div className={'contenitore'}>
+            <ShowCounterData counterValue={counter}/>
+            <button className={'più'} onClick={ increment }>+</button>
+            <button className={'meno'} disabled={ (counter <= 0) && 'disabled'} onClick={ decrease }>-</button>
+        </div>
+    );
+}
+
+Contatore.propTypes = {
+    valueStart: PropTypes.number.isRequired
+}
+
+Contatore.defaultProps = {
+    valueStart: 0
+}
+
+
+
+export default Contatore;
---
+++ componente/ShowCounterData.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function ShowCounterData (props){
+    const { counterValue } = props;
+
+    const test = 100 - counterValue;
+
+    return (
+        <div>
+            <p>Il tuo contatore vale: { counterValue}</p>
+            { (test > 0) && (
+                <p>Mancano {test} incrementi per arrivare a 100</p>
+            )}
+        </div>
+    );
+}
+
+ShowCounterData.propTypes = {
+    counterValue: PropTypes.number.isRequired
+}
+
+export default ShowCounterData;
